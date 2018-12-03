64a197b890069968df6ce677e635695aceb65d40
Ligęza <mligeza6@lab.metal.agh.edu.pl>
2018-12-03 14:23:16 +0100

    Offers database connection

--- WindowsFormsApp1/WindowsFormsApp1/Offers.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Offers.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LiteDB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,16 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApp1
 {
-    class Offer
+    public class Offer
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public double BasePrice { get; set; }
+        public string Place { get; set; }
+        public string Purpose { get; set; }
+        public DateTime Date { get; set; }
+
         public Offer(int id, DateTime date, string title, string content, double basePrice, string place, string purpose)
         {
             this.Id = id;
@@ -18,13 +27,11 @@ namespace WindowsFormsApp1
             this.Place = place;
             this.Purpose = purpose;
         }
-
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public double BasePrice { get; set; }
-        public string Place { get; set; }
-        public string Purpose { get; set; }
-        public DateTime Date { get; set; }
     }
+}
+
+using (var db = new LiteDatabase("DataBase.db"))
+{
+    var col = db.GetCollection<Offer>("offers");
+
 }
