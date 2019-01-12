9155444aa80dbfb5700403f85888a61e3b978644
unknown <marcin.ligeza2@gmail.com>
2019-01-12 18:17:34 +0100

    CRUD functions added to Offer class

--- WindowsFormsApp1/WindowsFormsApp1/Offers.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Offers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LiteDB;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,5 +34,52 @@ namespace WindowsFormsApp1
         public string Place { get; set; }
         public string Purpose { get; set; }
         public DateTime Date { get; set; }
+
+        public void addOffer(Offer offer)
+        {
+            using (var db = new LiteDatabase(@"Offers.db"))
+            {
+                var offers = db.GetCollection<Offer>("offers");
+
+                if (offers.FindById(offer.Id) == null)
+                {
+                    offers.Insert(offer);
+                }
+                else
+                {
+                    offers.Update(offer);
+                }
+
+            }
+        }
+
+        public Offer getOffer(int ID)
+        {
+            using (var db = new LiteDatabase(@"Offers.db"))
+            {
+                var offers = db.GetCollection<Offer>("offers");
+                return offers.FindById(ID);
+            }
+        }
+
+        public bool deleteOffer(int ID)
+        {
+            using (var db = new LiteDatabase(@"Offers.db"))
+            {
+                var offers = db.GetCollection<Offer>("offers");
+                return offers.Delete(ID);
+            }
+        }
+
+        public bool updateOffer(Offer offer)
+        {
+            using (var db = new LiteDatabase(@"Offers.db"))
+            {
+                var offers = db.GetCollection<Offer>("offers");
+                return offers.Update(offer);
+            }
+        }
+
+
     }
 }
